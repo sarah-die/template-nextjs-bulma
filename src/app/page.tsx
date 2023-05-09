8e5944dc0fd15df08d4d399558c0a3aee4a4b648
sarah-die <saridi@alice.de>
2023-05-09 13:35:24 +0200

    Add clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+import { useState } from "react";
+import clsx from "clsx";
+
 export default function Home() {
+  const [buttonSize, setButtonSize] = useState<boolean>(true);
+
   return (
     <main>
       <section className="section">
@@ -7,6 +13,14 @@ export default function Home() {
           <p className="subtitle">
             My first website with <strong>Bulma</strong>!
           </p>
+          <button
+            className={clsx("button", buttonSize ? "is-small" : "is-large")}
+            onClick={() => {
+              setButtonSize(!buttonSize);
+            }}
+          >
+            Button
+          </button>
         </div>
       </section>
     </main>
